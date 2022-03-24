c03fc067b716bb4e448ad019ee28f24ea3ebd643
olegpuzin <olegpuzin77@icloud.com>
2022-03-24 10:05:28 +0200

    _correction-JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
+    if (window.matchMedia('(min-width:768px)').matches) {
+        $('.video_play_icon').click(function() {
+            this.style.display = 'none';
+            $('.video_play_youtube-laptop').css('display', 'block');
+        });
+    }else {
+        $('.video_play_icon').click(function() {
+            this.style.display = 'none';
+            $('.video_play_youtube-mobile').css('display', 'block');
+        });
+    }
     $('.header_burger').click(function() {
         $('.header_burger').toggleClass('open-menu');
         $('.header_nav').toggleClass('open_nav');
     });
-    if (window.matchMedia('(min-width:768px)').matches) {
-        $('.icon-play').click(function() {
-            this.style.display = 'none';
-            $('.youtube-laptop').css('display', 'block');
-        });
-    }else {
-        $('.icon-play').click(function() {
-            this.style.display = 'none';
-            $('.youtube-mobile').css('display', 'block');
-        });
-    }
-    
 });
